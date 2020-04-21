c6cdd2da118cb5261de89e3ad1804c005bbcc4f5
Eriel Marimon <dev.eriel.mf@gmail.com>
2020-04-21 13:39:58 -0700

    Finished Welcome page template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from './page-components/dashboard/dashboard.component';
 import {JobDetailsPageComponent} from './page-components/job-details-page/job-details-page.component';
+import {LandingComponent} from './page-components/landing/landing.component';
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent},
+  {path: '', component: LandingComponent},
+  {path: 'landing', component: LandingComponent},
+  {path: 'welcome', component: LandingComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'job/:id', component: JobDetailsPageComponent}
 ];
--- src/app/page-components/landing/landing.component.ts
+++ src/app/page-components/landing/landing.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  whatThisIs = 'Scalable platform to create comprehensive databases from structured information in\n' +
+    'websites for marketing and AI research.';
+  howItWorks  = [
+    'Start a new scraping job in the dashboard by entering a web page\'s url.',
+    'The historic table should reflect a summary of the ongoing job.',
+    'The job will extract all Embedded Json in the page, as well as API calls it made and parse HTML tables.',
+    'Click on the Job row to see the extracted data and other details'
+  ];
+  links = [
+    {
+      link: 'https://example.com',
+      icon: 'facebook',
+      label: 'Facebook'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
